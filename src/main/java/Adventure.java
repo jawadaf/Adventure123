bfdbe8524e34a02cd9016ab105f7cfcc450e2453
muk2200 <mukh0001@stud.kea.dk>
2023-10-04 12:20:27 +0200

    Changes

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -3,46 +3,7 @@ public class Adventure {
     private Player player;
 
     public Adventure() {
-
         player = new Player(map.getCurrentRoom());
-    }
-
-    public void play() {
-    }
-
-    public void goEast(){
-        if (map.getCurrentRoom().getEast() != null) {
-            map.setCurrentRoom(map.getCurrentRoom().getEast());
-            System.out.println("You go east");
-        } else {
-            System.out.println("You can't go there");
-        }
-    }
-
-    public void goWest(){
-        if (map.getCurrentRoom().getWest() != null) {
-            map.setCurrentRoom(map.getCurrentRoom().getWest());
-            System.out.println("You go west");
-        } else {
-            System.out.println("You can't go there");
-        }
-    }
-    public void goNorth(){
-        if (map.getCurrentRoom().getNorth() != null) {
-            map.setCurrentRoom(map.getCurrentRoom().getNorth());
-            System.out.println("You go north");
-        } else {
-            System.out.println("You can't go there");
-        }
-
-    }
-    public void goSouth(){
-        if (map.getCurrentRoom().getSouth() != null) {
-            map.setCurrentRoom(map.getCurrentRoom().getSouth());
-            System.out.println("You go south");
-        } else {
-            System.out.println("You can't go there");
-        }
     }
 
     public Room getCurrentRoom() {
@@ -50,11 +11,32 @@ public class Adventure {
     }
 
     public String look() {
-        return map.look();
+        return player.getCurrentRoom().getDescription();
+        //der stod før map.look();
     }
 
     public boolean take(String userInput) {
         return player.takeItem(userInput);
+    }
 
+
+
+    public void move(String direction){
+        player.move(direction);
+    }
+    public void goEast(){
+        player.goEast();
+    }
+
+    public void goWest(){
+        player.goWest();
+    }
+
+    public void goSouth(){
+        player.goSouth();
+    }
+
+    public void goNorth(){
+        player.goNorth();
     }
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,12 +3,77 @@ import java.util.ArrayList;
 public class Player {
 
     private Room room;
+    private Map map;
     private ArrayList<Item> inventory;
+
+    public Room getCurrentRoom(){
+        return room;
+    }
 
     public Player(Room room){
 
         this.room = room;
         this.inventory = new ArrayList<>();
+    }
+
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+    public void move(String input) {
+
+        if (input.toLowerCase().equals("e")){
+            goEast();
+        } if (input.toLowerCase().equals("w")){
+            goWest();
+        }if (input.toLowerCase().equals("n")){
+            goNorth();
+        }if (input.toLowerCase().equals("s")){
+            goSouth();
+        }
+
+    }
+
+    public void goEast(){
+        if (room.getEast() != null) {
+            Room eastRoom = room.getEast();
+            System.out.println("You go east");
+            room = eastRoom;
+        } else {
+            System.out.println("You can't go there");
+        }
+    }
+
+    public void goWest(){
+        if (room.getWest() != null) {
+            Room westRoom = room.getWest();
+            System.out.println("You go west");
+            room = westRoom;
+        } else {
+            System.out.println("You can't go there");
+        }
+    }
+    public void goNorth(){
+        if (map.getCurrentRoom().getNorth() != null) {
+            map.setCurrentRoom(getCurrentRoom().getNorth());
+            System.out.println("You go north");
+        } else {
+            System.out.println("You can't go there");
+        }
+
+    }
+    public void goSouth(){
+        if (room.getSouth() != null) {
+            Room southRoom = room.getSouth();
+            room = southRoom;
+            System.out.println("You go south");
+        } else {
+            System.out.println("You can't go there");
+        }
     }
 
     public ArrayList<Item> getInventory() {
@@ -22,7 +87,7 @@ public class Player {
             if (item.getItemName().equals(requestedItem.toLowerCase())) {
                 int index = itemsInRoom.indexOf(item);
                 inventory.add(item);
-           //     room.removeItem(index);
+//                room.getItems()..RemoveItem();
                 itemFound = true;
                 break;
             }
